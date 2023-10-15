bc255754c8201d84702453594bdd9dd42102c1f0
kubni <nikola.kuburovic123@gmail.com>
2023-10-15 15:48:20 +0200

    feat: Parse incoming JSON from Hoogle
    In the current version, we get the json array from Hoogle as a
    Haskell String, then turn it into ByteString and finally use
    aeson's decode to turn it into an array of HoogleJsonResponses.
    FromJSON had to be instanced manually for it because of the
    field names (namely moduleInfo, packageInfo and typeInfo) being
    different from actual json keys. The difference was intentional
    in order to avoid keyword conflicts.

--- app/HoogleCommands.hs
+++ app/HoogleCommands.hs
@@ -10,8 +10,9 @@ where
 import Data.Aeson
 import Data.Text
 import GHC.Generics
-
 import Network.Curl
+
+import Data.ByteString.Lazy.Char8 as BS
 
 data ModuleInfo = ModuleInfo {
                                 name :: String,
@@ -41,20 +42,24 @@ instance FromJSON PackageInfo
 instance ToJSON PackageInfo where
   toEncoding = genericToEncoding defaultOptions
 
-instance FromJSON HoogleJsonResponse
+instance FromJSON HoogleJsonResponse where
+  parseJSON = withObject "HoogleJsonResponse" $ \o -> do
+    docs <- o .: "docs"
+    item <- o .: "item"
+    moduleInfo <- o .: "module"
+    packageInfo <- o .: "package"
+    typeInfo <- o .: "type"
+    url <- o .: "url"
+    return $ HoogleJsonResponse docs item moduleInfo packageInfo typeInfo url
+
 instance ToJSON HoogleJsonResponse where
   toEncoding = genericToEncoding defaultOptions
 
 
--- Manual way:
--- instance FromJSON ModuleInfo where
---   parseJSON = withObject "ModuleInfo" $ \o -> do
---     name <- o .: "name"
---     url <- o .: "url"
---     return $ ModuleInfo name url
-
 testCurl = do
-  pair <- curlGetString "https://hoogle.haskell.org?mode=json&hoogle=map&start=1&count=2" []
-  let jsonString = snd pair
-  let encodedJson = encode jsonString
-  print encodedJson
+  response <- curlGetString "https://hoogle.haskell.org?mode=json&hoogle=map&start=1&count=2" []
+  let jsonArrayString = snd response
+  -- let encodedJson = encode jsonArrayString
+  let byteStringJson = BS.pack jsonArrayString
+  let hoogleResponse = eitherDecode byteStringJson :: Either String [HoogleJsonResponse]
+  print hoogleResponse
