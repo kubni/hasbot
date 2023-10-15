7445f84bf2997938a691140e2863856dff5a4609
kubni <nikola.kuburovic123@gmail.com>
2023-10-15 12:32:12 +0200

    feat: Add some simple message parsing
    There is a lot of room for improvement here

--- app/Main.hs
+++ app/Main.hs
@@ -1,36 +1,47 @@
-{-# LANGUAGE OverloadedStrings #-}  -- allows "string literals" to be Text
+{-# LANGUAGE OverloadedStrings #-}
 import           Control.Monad (when, void)
+import           Control.Monad.IO.Class (liftIO)
 import           UnliftIO.Concurrent
-import           Data.Text (isPrefixOf, toLower, Text)
+import           Data.Text (isPrefixOf, toLower, Text, unpack)
 import qualified Data.Text.IO as TIO
 
 import           Discord
 import           Discord.Types
 import qualified Discord.Requests as R
 
--- | Replies "pong" to every message that starts with "ping"
-pingpongExample :: IO ()
-pingpongExample = do
-    userFacingError <- runDiscord $ def
-             { discordToken = ""
-             , discordOnEvent = eventHandler
-             , discordOnLog = \s -> TIO.putStrLn s >> TIO.putStrLn ""
-             } -- if you see OnLog error, post in the discord / open an issue
-
-    TIO.putStrLn userFacingError
-    -- userFacingError is an unrecoverable error
-    -- put normal 'cleanup' code in discordOnEnd (see examples)
+main :: IO ()
+main = do
+  botToken <- TIO.readFile "most_secret_token_ever"
+  userFacingError <- runDiscord $ def
+           { discordToken = botToken
+           , discordOnEvent = eventHandler
+           , discordOnLog = \s -> TIO.putStrLn s >> TIO.putStrLn ""
+           }
+  TIO.putStrLn userFacingError
 
 eventHandler :: Event -> DiscordHandler ()
 eventHandler event = case event of
-    MessageCreate m -> when (isPing m && not (fromBot m)) $ do
-        void $ restCall (R.CreateReaction (messageChannelId m, messageId m) "eyes")
-        threadDelay (2 * 10^6)
-        void $ restCall (R.CreateMessage (messageChannelId m) "Pong!")
-    _ -> return ()
+        MessageCreate m -> when (isCommand m) $ do
+          liftIO $ parseMessage m
+          void $ restCall (R.CreateMessage (messageChannelId m) "Pong!")
+        _ -> return ()
 
-fromBot :: Message -> Bool
-fromBot = userIsBot . messageAuthor
 
-isPing :: Message -> Bool
-isPing = ("ping" `isPrefixOf`) . toLower . messageContent
+
+-- TODO: Monadic function chains ?
+parseMessage :: Message -> IO ()
+parseMessage m = when (isCommand m) $ do
+  let msg = unpack $ messageContent m
+  let cmdParts = tail $ words msg
+  let response = if head cmdParts /= "please" then return "You have to be more polite to Hasbot." else tail cmdParts
+  print response
+
+
+
+
+notFromBot :: Message -> Bool
+notFromBot m = not $ userIsBot (messageAuthor m)
+
+isCommand :: Message -> Bool
+-- isCommand m = "/" `isPrefixOf` (messageContent m)
+isCommand m = "Hasbot, " `isPrefixOf` (messageContent m)
